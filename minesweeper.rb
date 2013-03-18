d35c74c4c797bac344c22dccd78659cb67b5f176
Joshua <Joshua.J.Wilborn@gmail.com>
2013-03-18 16:27:44 -0700

    Game works, still need to add high score functionality.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,6 +1,6 @@
 require 'debugger'
 class Game
-
+  attr_accessor :board
   def initialize(size, mines)
     @board = Board.new(size, mines)
   end
@@ -8,48 +8,81 @@ class Game
   #loops through the game until over
   def play
 
-    until over?
+    until won?
+      @board.print
+
       puts "Please enter your move: (h for help)"
 
-      prompt(gets.chomp)
+      input = prompt(gets.chomp)
+
+      next if input.nil?
+
+      make_move(input)
+
+
+
 
     end
-
+    @board.print
+    puts "YOU WON!!!"
   end
 
+  def make_move(input)
+    # input = ['reveal',4,5]
+    row, col = input[1], input[2]
+    if input[0][0] = 'r'
+      if @board.bomb?(row,col)
+        puts "KABOOM!"
+        exit
+      else
+        @board.board_reveal[row][col] = true
+      end
+    else
+      # it has to be flag
+      unless @board.board_flag.include?([row,col])
+        @board.board_flag << [row,col]
+      else
+        @board.board_flag.delete([row,col])
+      end
+    end
+  end
+
+
   def prompt(input)
-    input = input.split.map(&:downcase)
-    input[1..2].map!(&:to_i)
+    input = input.split(' ').map(&:downcase)
+    input[1],input[2] = input[1..2].map!(&:to_i)
     if input[0][0] == 'h'
       puts "Enter your move as 'Reveal row column' or "
       puts "Enter your move as 'Flag row column' "
       puts "Enter 'q' to quit"
-      return
+      return nil
     elsif valid_move?(input)
-      # input = ['Flag', 3, 4]
-      make_move(input)
+      return input
     elsif input[0][0] == 'q'
       puts "Too tough for ya?"
       exit
     else
       puts "Please enter valid move."
+      p input
+      return nil
     end
-
   end
 
 
   def valid_move?(input)
-      (input[0][0] == 'r' || 'f' ) && (0..(Board.size-1)).include?(input[1]) && (0..(Board.size-1)).include?(input[2])
+      (input[0][0] == 'r' || input[0][0] == 'f' ) && (0..(@board.size-1)).include?(input[1]) && (0..(@board.size-1)).include?(input[2])
   end
 
-  def over?
-    # boolean to check if game over
-    # all non-bomb squares revealed or bomb explode
+  def won?
+    return true if @board.board_reveal.flatten.select{|bool| bool == false }.length == @board.mines
+    false
   end
 
 end
 
 class Board
+
+  attr_accessor :size, :board_reveal, :board_flag, :mines
 
   def initialize(size, mines)
     @size = size
@@ -91,6 +124,7 @@ class Board
     neighbors_of_rc = @NEIGHBORS.map {|r,c| [r+row,c+col] }
     neighbors_of_rc.each do |cell|
       next if @board[cell[0]].nil? || @board[cell[0]][cell[1]].nil?
+      next if cell[0] < 0  || cell[1] < 0
       @board[cell[0]][cell[1]] += 1 unless bomb?(cell[0],cell[1])
     end
 
@@ -107,11 +141,16 @@ class Board
       row.each_with_index do |element, c_i|
         if @board_reveal[r_i][c_i]
           print_board[r_i][c_i] = element
+          print_board[r_i][c_i] = 'F' if @board_flag.include?([[r_i],[c_i]])
           print_board[r_i][c_i] = '_' if @board[r_i][c_i] == 0
         end
       end
     end
-  print_board
+    puts (" " * 20) + ( "_" * (10+@size) )
+    print_board.each do |line|
+      puts (" " * 20) + "|" + line.join(" ") + "|"
+    end
+    puts (" " * 20 ) + "_" * (10+@size)
   end
 
 end
